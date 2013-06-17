59c5719fcd3e1ae6382782c732139cc9d52c27d1
Jack <jackp@vayoungs.us>
2013-06-16 20:04:57 -0400

    add generic routes to js

--- application/views/uploader_view.php
+++ application/views/uploader_view.php
@@ -9,6 +9,12 @@
 	<meta name="author" content="jack young">
 	<!-- Date: 2013-06-15 -->
 	
+	<script type="text/javascript">
+	  var baseurl = "<?php print base_url(); ?>index.php/";
+	  var image_base_url = "<?php print base_url(); ?>";
+	</script>
+	
+	
 	<script type="text/javascript" src="<?php echo base_url(); ?>js/uploader_home.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 	
